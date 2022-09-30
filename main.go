0978adb13e24498e3c71259721cafbcf74c43682
rogerio <rrobetti@gmail.com>
2022-09-30 17:30:19 +0100

    Example of grpc server starting up without errors.

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
-import "igbodb/leader-writter"
-import "igbodb/load-manager"
-import "igbodb/read-replica"
+import (
+	"log"
+	"net"
+    "fmt"
+	"igbodb/leader-writter"
+    "igbodb/load-manager"
+    "igbodb/read-replica"
+    "igbodb/server"
+)
 
 func main() {
 	fmt.Println("From main function..")
 	leaderwritter.Print()
 	loadmanager.Print()
 	readreplica.Print()
+
+	log.Println("Starting listening on port 8080")
+     port := ":8080"
+
+     lis, err := net.Listen("tcp", port)
+     if err != nil {
+      log.Fatalf("failed to listen: %v", err)
+     }
+     log.Printf("Listening on %s", port)
+     srv := server.NewGRPCServer()
+
+     if err := srv.Serve(lis); err != nil {
+      log.Fatalf("failed to serve: %v", err)
+     }
 }
---
+++ server/activity.go
@@ -0,0 +1,97 @@
+package server
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"time"
+
+	api "igbodb/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	// needed for SQLite driver
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const create string = `
+		CREATE TABLE IF NOT EXISTS activities (
+		id INTEGER NOT NULL PRIMARY KEY,
+		time DATETIME NOT NULL,
+		description TEXT
+		);
+`
+const file string = "activities.db"
+
+type Activities struct {
+	db *sql.DB
+}
+
+func NewActivities() (*Activities, error) {
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(create); err != nil {
+		return nil, err
+	}
+	return &Activities{
+		db: db,
+	}, nil
+}
+func (c *Activities) Insert(activity *api.Activity) (int, error) {
+	res, err := c.db.Exec("INSERT INTO activities VALUES(NULL,?,?);", activity.Time.AsTime(), activity.Description)
+	if err != nil {
+		return 0, err
+	}
+
+	var id int64
+	if id, err = res.LastInsertId(); err != nil {
+		return 0, err
+	}
+	log.Printf("Added %v as %d", activity, id)
+	return int(id), nil
+}
+
+var ErrIDNotFound = errors.New("Id not found")
+
+func (c *Activities) Retrieve(id int) (*api.Activity, error) {
+	log.Printf("Getting %d", id)
+
+	// Query DB row based on ID
+	row := c.db.QueryRow("SELECT id, time, description FROM activities WHERE id=?", id)
+
+	// Parse row into Interval struct
+	activity := api.Activity{}
+	var err error
+	var time time.Time
+	if err = row.Scan(&activity.Id, &time, &activity.Description); err == sql.ErrNoRows {
+		log.Printf("Id not found")
+		return &api.Activity{}, ErrIDNotFound
+	}
+	activity.Time = timestamppb.New(time)
+	return &activity, err
+}
+
+func (c *Activities) List(offset int) ([]*api.Activity, error) {
+	log.Printf("Getting list from offset %d\n", offset)
+
+	// Query DB row based on ID
+	rows, err := c.db.Query("SELECT * FROM activities WHERE ID > ? ORDER BY id DESC LIMIT 100", offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	data := []*api.Activity{}
+	for rows.Next() {
+		i := api.Activity{}
+		var time time.Time
+		err = rows.Scan(&i.Id, &time, &i.Description)
+		if err != nil {
+			return nil, err
+		}
+		i.Time = timestamppb.New(time)
+		data = append(data, &i)
+	}
+	return data, nil
+}
